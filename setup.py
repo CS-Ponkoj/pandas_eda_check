c9f7bffe960ac5344112bd9d60ee1bc4b06bf5f2
CS-Ponkoj <csponkoj@gmail.com>
2025-06-23 00:17:27 -0700

    Initial commit: EDA check function

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from setuptools import setup, find_packages
 
+# Load README content
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='pandas_eda_check',
-    version='0.1.0',
+    version='0.1.1',  # bump version to update on PyPI
     packages=find_packages(),
     install_requires=['pandas'],
     description='Quick EDA utility to summarize unique and missing values in pandas DataFrames.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Ponkoj Shill',
     author_email='csponkoj@gmail.com',
     url='https://github.com/CS-Ponkoj/pandas_eda_check',
